acf09ee29e1fc3578483df908cbd0c589e031166
YuraYosikage <yudhayudhistira730@gmail.com>
2025-09-05 09:57:23 +0700

    Lay Out design + Code

---
+++ MusicPlayerProject/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MusicPlayerProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
